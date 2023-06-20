84279847dc889f97b1e7552b365b80f3aa2c1245
jdkeke142 <kjimenez@student.42angouleme.fr>
2023-06-20 20:24:09 +0200

    Updated libft module

--- src/server.c
+++ src/server.c
@@ -6,67 +6,19 @@
 /*   By: kjimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 18:23:09 by kjimenez          #+#    #+#             */
-/*   Updated: 2023/06/20 18:26:16 by kjimenez         ###   ########.fr       */
+/*   Updated: 2023/06/20 20:08:56 by kjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <ft_string.h>
+#include <ft_stdio.h>
 #include <stdlib.h>
 #include <ft_math.h>
 
 char	*g_binary_str;
-
-static int	ft_substr_len(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < len && s[start + i])
-		i++;
-	return (i);
-}
-
-static char	*substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	size_t	malloc_len;
-	char	*sub_str;
-
-	if (!s || start >= strlen(s))
-		return (strdup(""));
-	malloc_len = ft_substr_len(s, start, len);
-	sub_str = (char *) malloc((malloc_len + 1) * sizeof(char));
-	if (!sub_str)
-		return (NULL);
-	i = 0;
-	while (i < len && s[start + i])
-	{
-		sub_str[i] = s[start + i];
-		i++;
-	}
-	sub_str[i] = '\0';
-	return (sub_str);
-}
-
-void	*ft_realloc(void *ptr, size_t newsize)
-{
-	char	*newptr;
-	size_t	cursize;
-
-	if (ptr == 0)
-		return (malloc(newsize));
-	cursize = sizeof(ptr);
-	if (newsize <= cursize)
-		return (ptr);
-	newptr = malloc(newsize);
-	memcpy(ptr, newptr, cursize);
-	free(ptr);
-	return (newptr);
-}
 
 static char	*btoa(char *str)
 {
@@ -76,12 +28,12 @@ static char	*btoa(char *str)
 	int					i;
 	int					dec;
 
-	a_str = malloc((strlen(str) / 8) * sizeof(char));
+	a_str = malloc((ft_strlen(str) / 8) * sizeof(char));
 	a_str[0] = '\0';
 	substr_start = 0;
-	while (substr_start < strlen(str))
+	while (substr_start < ft_strlen(str))
 	{
-		octect = substr(str, substr_start, 8);
+		octect = ft_substr(str, substr_start, 8);
 		dec = 0;
 		i = 0;
 		while (i < 8)
@@ -90,7 +42,7 @@ static char	*btoa(char *str)
 				dec += 1 * ft_pow(2, 8 - 1 - i);
 			i++;
 		}
-		strcat(a_str, (char *) &dec);
+		ft_strcat(a_str, (char *) &dec);
 		substr_start += 8;
 		free(octect);
 	}
@@ -109,9 +61,9 @@ static void	handle_binary_1(int sig, siginfo_t *info, void *context)
 	else
 	{
 		g_binary_str = realloc(g_binary_str,
-				strlen(g_binary_str) + 2 * sizeof(char));
+				ft_strlen(g_binary_str) + 2 * sizeof(char));
 	}
-	strcat(g_binary_str, "1\0");
+	ft_strcat(g_binary_str, "1");
 	kill(info->si_pid, SIGUSR1);
 }
 
@@ -128,14 +80,14 @@ static void	handle_binary_0(int sig, siginfo_t *info, void *context)
 	}
 	else
 		g_binary_str = realloc(g_binary_str,
-				strlen(g_binary_str) + 2 * sizeof(char));
-	strcat(g_binary_str, "0\0");
-	if (strlen(g_binary_str) >= 8
-		&& !strcmp(g_binary_str + strlen(g_binary_str) - 8, "00000000")
-		&& strlen(g_binary_str) % 8 == 0)
+				ft_strlen(g_binary_str) + 2 * sizeof(char));
+	ft_strcat(g_binary_str, "0");
+	if (ft_strlen(g_binary_str) >= 8
+		&& !ft_strcmp(g_binary_str + ft_strlen(g_binary_str) - 8, "00000000")
+		&& ft_strlen(g_binary_str) % 8 == 0)
 	{
 		a_str = btoa(g_binary_str);
-		printf("Message received from client: %s\n", a_str);
+		ft_printf("Message received from client: %s\n", a_str);
 		free(a_str);
 		free(g_binary_str);
 		g_binary_str = NULL;
@@ -157,7 +109,7 @@ int	main(void)
 	binary0_sig.sa_flags = SA_SIGINFO;
 	sigemptyset(&binary0_sig.sa_mask);
 	sigaction(SIGUSR2, &binary0_sig, NULL);
-	printf("Server PID is : %d\n", getpid());
+	ft_printf("Server PID is : %d\n", getpid());
 	while (1)
 		;
 }
