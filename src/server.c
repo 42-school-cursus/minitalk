60dd863dc8e9e6684279650abc93f024b99dc02c
jdkeke142 <kjimenez@student.42angouleme.fr>
2023-06-23 01:06:02 +0200

    Minitalk progress

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: kjimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 18:23:09 by kjimenez          #+#    #+#             */
-/*   Updated: 2023/06/23 00:51:21 by kjimenez         ###   ########.fr       */
+/*   Updated: 2023/06/23 01:05:42 by kjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,47 +20,28 @@
 //proteger malloc
 //relink makefile
 
-char	*ft_straddc_first(char c)
+static void	append_char(char **str, char c)
 {
-	char	*add;
+	size_t	str_len;
 
-	add = (char *)malloc(sizeof(char) * 2);
-	if (!add)
-		return (NULL);
-	add[0] = c;
-	add[1] = '\0';
-	return (add);
-}
-
-char	*ft_straddc(char *str, char c)
-{
-	char	*add;
-	int		i;
-
-	if (!c)
-		return (NULL);
-	if (!str)
-		return (ft_straddc_first(c));
-	add = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2));
-	if (!add)
+	if (!*str)
 	{
-		free(str);
-		return (NULL);
+		*str = malloc(2);
+		(*str)[0] = '\0';
 	}
-	i = -1;
-	while (str[++i])
-		add[i] = str[i];
-	free(str);
-	add[i++] = c;
-	add[i] = '\0';
-	return (add);
+	else
+	{
+		str_len = ft_strlen(*str);
+		*str = ft_realloc((void **) str, str_len + 1, str_len + 2);
+	}
+	ft_strcat(*str, &c);
 }
 
-static void	print_message(char **message)
+static void	print_message(char **str)
 {
-	ft_printf("Message received from client : %s\n", *message);
-	free(*message);
-	*message = NULL;
+	ft_printf("Message received from client : %s\n", *str);
+	free(*str);
+	*str = NULL;
 }
 
 static void	handle_binary(int sig, siginfo_t *info, void *context)
@@ -81,15 +62,7 @@ static void	handle_binary(int sig, siginfo_t *info, void *context)
 			print_message(&a_str);
 		else
 		{
-			if (a_str == NULL)
-			{
-				a_str = malloc(2);
-				a_str[0] = '\0';
-			}
-			else
-				a_str = ft_realloc((void **) &a_str, ft_strlen(a_str) + 1, ft_strlen(a_str) + 2);
-			ft_strcat(a_str, &c);
-			ft_printf("New version\n");
+			append_char(&a_str, c);
 		}
 		bits = 0;
 		c = (char) 255;
